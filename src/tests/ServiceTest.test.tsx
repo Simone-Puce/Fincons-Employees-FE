3b83fc5dd1faca218ec697baedbf17c373af9465
simone.puce <simone.puce@finconsgroup.com>
2023-11-30 14:30:55 +0100

    test

--- src/tests/ServiceTest.test.tsx
+++ src/tests/ServiceTest.test.tsx
@@ -1,19 +1,11 @@
-import axios from "axios";
 import EmployeeService from "../services/EmployeeService";
 
-jest.mock('axios');
+const axios = require('axios')
+const employees = EmployeeService.getEmployeesById(402)
 
-it("should return success if the employee list is empty" ,async() => {
-  const mockResponse = {
-    data: {},
-  };
-  
-  axios.get = jest.fn().mockResolvedValue(mockResponse)
-  let employees;
-  let tempData = await axios.get("http://localhost:8080/api/v1/employees").then(res=>{employees=res.data})
-  console.log(tempData)
+jest.mock('axios')
 
-  let temp = EmployeeService.getEmployees().then(res=>console.log(res.data))
-  expect(mockResponse.data).toEqual(employees)
-  //expect(temp).toHaveBeenCalled();
-});
+it("should do something",async()=>{
+  axios.get.mockResolvedValue({})
+  employees;
+})
