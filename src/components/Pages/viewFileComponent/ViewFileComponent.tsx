657d65ce423b387bda17e2688995f8849b4a7f32
michelangelo.masiell <michelangelo.masiello@finconsgroup.com>
2024-01-19 17:03:05 +0100

    fixed filecomponent

--- src/components/Pages/viewFileComponent/ViewFileComponent.tsx
+++ src/components/Pages/viewFileComponent/ViewFileComponent.tsx
@@ -20,30 +20,18 @@ const ViewFileComponent = () => {
         <div>
             <div className="mt-5 pt-3">
                 <div className="card col-md-6 offset-md-3 mt-5"></div>
-                <tbody>
-                    {
-                        employee?.fileList?.map(singleFile => (
-                            <tr className="text-center">
-                                <td>{singleFile.id}</td>
-                                <td>{singleFile.name}</td>
-                                <td>{singleFile.description}</td>
-                                <td>{singleFile.extension}</td>
-                                <div className="btn-group m-2" role="group">
-                                    <button
-                                        type="button"
-                                        className="btn btn-primary"
-                                        onClick={goToFileListEmp}>
-                                        {" "}
-                                        Go back to the employee list
-                                    </button>
-                                </div>
-                            </tr>
-                        ))
-                    }
-                </tbody>
-                <Link to={`/file/list/${employee?.id}`}><button type="button" className="btn btn-secondary">Go back to {employee?.firstName}'s file list</button></Link>
 
+                <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={goToFileListEmp}>
+                    {" "}
+                    Go back to the employee list
+                </button>
             </div>
+
+            <Link to={`/file/list/${employee?.id}`}><button type="button" className="btn btn-secondary">Go back to {employee?.firstName}'s file list</button></Link>
+
         </div>
     )
 }
